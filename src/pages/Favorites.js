78b01e807026b48bcfbc62d0b01e4d2c8d5b912c
Joel Inman <joel.inman@nextpixel.com>
2023-06-20 16:08:37 +0100

    React Course complete!

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,10 +1,37 @@
-const title = "Favorites Page";
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+import { Link } from "react-router-dom";
+const title = "My Favorites";
 
 function FavoritesPage() {
+  const favoritesCtx = useContext(FavoritesContext);
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = (
+      <h3>
+        {"You do not have any favorites, "}{" "}
+        <Link to="/">{"add some now!"}</Link>
+      </h3>
+    );
+  } else {
+    content = (
+      <div>
+        <MeetupList meetups={favoritesCtx.favorites} />
+        <h4>
+          {"That's your lot, do you want to "}
+          <Link to="/">{"Add More?"}</Link>
+        </h4>
+      </div>
+    );
+  }
+
   return (
-    <div>
+    <section>
       <h1>{title}</h1>
-    </div>
+      {content}
+    </section>
   );
 }
 
